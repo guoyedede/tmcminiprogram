690fbee63e15589ede7b36503a7170367b12c1cb
guoyedede@126.com <guoyedede@126.com>
2019-03-20 13:44:27 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
@@ -9,6 +9,18 @@ Page({
 
   onLoad: function () {
     //officer id
-    this.setData({isOfficer: true});
+    this.setData({ isOfficer: true });
+  },
+
+  gotoCheckin: function (event) {
+    wx.navigateTo({
+      url: '../user/checkin',
+    });
+  },
+
+  gotoSchedule: function(event) {
+    wx.navigateTo({
+      url: '../user/schedule',
+    });
   }
 })
